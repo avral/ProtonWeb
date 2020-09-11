c54043502c9ae809a2d99f12e58fd50bfc752e8a
jafri <syed.jafri2@ucalgary.ca>
2020-09-11 17:18:35 -0600

    Initial commit

---
+++ packages/proton-connect/lib/index.js
@@ -0,0 +1,10 @@
+import AnchorLink from 'anchor-link';
+import ProtonLinkBrowserTransport from '@protonprotocol/proton-browser-transport';
+export const ProtonConnect = (linkOptions = {}, transportOptions = {}) => {
+    const transport = new ProtonLinkBrowserTransport(transportOptions);
+    return new AnchorLink({
+        transport,
+        ...linkOptions
+    });
+};
+//# sourceMappingURL=index.js.map
