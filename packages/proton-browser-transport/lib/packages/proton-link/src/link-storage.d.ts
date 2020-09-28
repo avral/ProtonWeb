872cd9daf5a2c2a88bea092996f5651a2a4f6816
jafri <syed.jafri2@ucalgary.ca>
2020-09-28 17:37:24 -0600

    rm lib

--- packages/proton-browser-transport/lib/packages/proton-link/src/link-storage.d.ts
+++
@@ -1,6 +0,0 @@
-export interface LinkStorage {
-    write(key: string, data: string): Promise<void>;
-    read(key: string): Promise<string | null>;
-    remove(key: string): Promise<void>;
-}
-//# sourceMappingURL=link-storage.d.ts.map
--- packages/proton-browser-transport/lib/packages/proton-signing-request/src/base64u.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function encode(data: Uint8Array): string;
-export declare function decode(input: string): Uint8Array;
-//# sourceMappingURL=base64u.d.ts.map
